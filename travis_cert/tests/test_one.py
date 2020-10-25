6759a46dcefcdc5719fcec64ccc56abdd34cef40
Fernando La Chica <fernando.lachica@guadaltech.es>
2020-10-25 14:02:19 +0100

    Success attempt

--- travis_cert/tests/test_one.py
+++ travis_cert/tests/test_one.py
@@ -3,6 +3,6 @@ from odoo.tests.common import TransactionCase
 
 class TestCase(TransactionCase):
     def test_compute(self):
-        record = self.env["test.case"].create({"name": "test", "value": 3, "value2": 2})
+        record = self.env["test.case"].create({"name": "test", "value": 3, "value2": 3})
         record.compute()
         self.assertEqual(record.result, 9)
