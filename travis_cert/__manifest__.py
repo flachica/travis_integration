cc7b343bd16d0ce16a2b4e283144918f212aba46
Fernando La Chica <fernando.lachica@guadaltech.es>
2020-10-25 13:42:57 +0100

    Simplifying things to failed attemp

--- travis_cert/__manifest__.py
+++ travis_cert/__manifest__.py
@@ -1,21 +1,13 @@
 {
-    'name': "travis_cert",
-
-    'summary': """
+    "name": "travis_cert",
+    "summary": """
     To send message to custom controller to be informed from the test's results
     """,
-
-    'author': "Fernando La Chica",
-    'website': "https://flachica.github.io/fernandolachica/",
-
-    'category': 'Human Resources',
-    'version': '13.0.0.1.0',
-
-    'depends': ['base'],
-
-    'data': [
-        'security/ir.model.access.csv',
-        'views/test_case.xml',
-    ],
-    'application': True,
+    "author": "Fernando La Chica",
+    "website": "https://flachica.github.io/fernandolachica/",
+    "category": "Human Resources",
+    "version": "13.0.0.1.0",
+    "depends": ["base"],
+    "data": ["security/ir.model.access.csv", "views/test_case.xml"],
+    "application": True,
 }
--- travis_cert/models/test_case.py
+++ travis_cert/models/test_case.py
@@ -1,14 +1,14 @@
-from odoo import models, fields, api
+from odoo import fields, models
 
 
-class test_case(models.Model):
-    _name = 'test.case'
-    _description = 'Testing cases'
+class TestCase(models.Model):
+    _name = "test.case"
+    _description = "Testing cases"
 
     name = fields.Char()
-    value = fields.Integer('Value 1')
-    value2 = fields.Integer('Value 2')
-    result = fields.Integer('Result')
+    value = fields.Integer("Value 1")
+    value2 = fields.Integer("Value 2")
+    result = fields.Integer("Result")
 
     def compute(self):
         self.ensure_one()
